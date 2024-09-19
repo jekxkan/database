6fadab9b0ed9fa8c36b28db07966f1023043d726
jekxkan <smirnova.eyu@mail.ru>
2024-09-19 12:23:26 +0300

    Add changes

--- main.py
+++ main.py
@@ -1,30 +1,51 @@
 import psycopg2
 
-#Подключение к БД, предварительно созданной в SQL Shell
-connection = psycopg2.connect(
-    host = '127.0.0.1',
-    user = 'postgres',
-    password = 'zxcvb',
-    database = 'company_db'
-)
-#Автоматическое внесение изменений в БД
-connection.autocommit = True
 
-with connection.cursor() as cursor:
-    "Создание таблицы работников"
+try:
+    #Подключение к БД, предварительно созданной в SQL Shell
+    connection = psycopg2.connect(
+        host = '127.0.0.1',
+        user = 'postgres',
+        password = 'zxcvb',
+        database = 'company_db'
+    )
+    #Автоматическое внесение изменений в БД
+    connection.autocommit = True
 
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS employees(
-            id SERIAL PRIMARY KEY,
+    with connection.cursor() as cursor:
+        "Создание таблицы работников"
+
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS employees(
+                id SERIAL PRIMARY KEY,
             first_name VARCHAR(50),
             last_name VARCHAR(50),
             age INT,
             department VARCHAR(50),
             salary NUMERIC);
+            """
+        )
+    def create_employee(id, first_name, last_name, age, department, salary):
         """
-    )
-def create_employee(id, first_name, last_name, age, department, salary):
-    with connection.cursor() as cursor:
-        query = "INSERT INTO employees(id, first_name, last_name, age, department, salary) VALUES (%s);"
-        cursor.execute(query, (id, first_name, last_name, age, department, salary))
-create_employee(1, 'sf', 'sdf', 4, 'sdf', 6)
+        Добавление нового сотрудника в БД
+
+        Args:
+            id(int) : уникальный id сотрудника
+            first_name(str): имя сотрудника
+            last_name(str): фамилия сотрудника
+            age(int): возраст сотрудника
+            department(str): подразделение, в котором работает сотрудник
+            salary: зарплата сотрудника
+        """
+        with connection.cursor() as cursor:
+            keys = 'id, first_name, last_name, age, department, salary'
+            query = f"INSERT INTO employees({keys}) VALUES ({id}, '{first_name}', '{last_name}', {age}, '{department}', {salary});"
+            cursor.execute(query, (id, first_name, last_name, age, department, salary))
+except Exception as e:
+    print(f"Error {e}")
+
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Connection closed")
