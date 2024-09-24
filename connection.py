dcd7da7f84f5e85ca8354e49ec4ecdd076775f36
jekxkan <smirnova.eyu@mail.ru>
2024-09-24 18:48:59 +0300

    Don't work...

--- connection.py
+++ connection.py
@@ -3,10 +3,11 @@ from psycopg2 import pool
 
 #Подключение к БД после docker-compose
 postgresql_pool = psycopg2.pool.SimpleConnectionPool(1, 20,
-    host = '127.0.0.1',
-    user = 'postgres',
-    password = 'zxcvb',
-    database = 'company_db'
+    host = 'localhost',
+    user = 'admin',
+    port = '5432',
+    password = 'root',
+    dbname = 'company_db'
 )
 
 #Получаем соединение из пула соединений
