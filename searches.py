05d69fbad79feea547f8412234f40b8baf96fb8d
jekxkan <smirnova.eyu@mail.ru>
2024-09-25 20:18:08 +0300

    Some

--- searches.py
+++ searches.py
@@ -1,70 +1,74 @@
 from connection import connection
 
-def search_by_name_and_surname(name: str, surname: str) -> [tuple]:
+class Searches:
     """
-    Выводит информацию о сотруднике по имени и фамилии
-
-    Args:
-        name(str): имя сотрудника
-        surname(str): фамилия сотруника
-
-    Returns:
-        список кортежей, в которых содержится информация о сотрудниках с введенными именем и фамилией
+    Класс с методами для поиска информации в БД
     """
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """
-            SELECT * 
-            FROM employees
-            WHERE first_name = %s 
-            AND last_name = %s;
-            """, (name, surname)
-        )
-        return cursor.fetchall()
+    def search_by_name_and_surname(name: str, surname: str) -> [tuple]:
+        """
+        Выводит информацию о сотруднике по имени и фамилии
+
+        Args:
+            name(str): имя сотрудника
+            surname(str): фамилия сотруника
+
+        Returns:
+            список кортежей, в которых содержится информация о сотрудниках с введенными именем и фамилией
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT * 
+                FROM employees
+                WHERE first_name = %s 
+                AND last_name = %s;
+                """, (name, surname)
+            )
+            return cursor.fetchall()
 
 
-def search_above_average_salary() -> [tuple]:
-    """
-    Выводит информацию о сотрудниках, чья зарплата выше среднего
+    def search_above_average_salary() -> [tuple]:
+        """
+        Выводит информацию о сотрудниках, чья зарплата выше среднего
 
-    Returns:
-        список кортежей, в которых содержится информация о сотрудниках с зарплатой выше среднего
-    """
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """
-            SELECT AVG(salary)
-            FROM employees;
-            """
-        )
-        average_salary = list(cursor.fetchone())[0]
-        cursor.execute(
-            f"""
-            SELECT *
-            FROM employees
-            WHERE salary > %s;
-            """, (average_salary,)
-        )
-        return cursor.fetchall()
+        Returns:
+            список кортежей, в которых содержится информация о сотрудниках с зарплатой выше среднего
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT AVG(salary)
+                FROM employees;
+                """
+            )
+            average_salary = list(cursor.fetchone())[0]
+            cursor.execute(
+                f"""
+                SELECT *
+                FROM employees
+                WHERE salary > %s;
+                """, (average_salary,)
+            )
+            return cursor.fetchall()
 
 
-def search_by_department(department: str) -> [tuple]:
-    """
-    Выводит сотрудников, которые работают в отделе
+    def search_by_department(department: str) -> [tuple]:
+        """
+        Выводит сотрудников, которые работают в отделе
 
-    Args:
-        department(str): отдел
+        Args:
+            department(str): отдел
 
-    Returns:
-        список кортежей, в которых содержится информация о сотрудниках, работающих в отделе
-    """
-    with connection.cursor() as cursor:
-        cursor.execute(
-            f"""
-            SELECT *
-            FROM employees
-            WHERE department = %s
-            ORDER BY id;
-            """, (department,)
-        )
-        return cursor.fetchall()
+        Returns:
+            список кортежей, в которых содержится информация о сотрудниках, работающих в отделе
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(
+                f"""
+                SELECT *
+                FROM employees
+                WHERE department = %s
+                ORDER BY id;
+                """, (department,)
+            )
+            return cursor.fetchall()
--- service_functions.py
+++ service_functions.py
@@ -1,6 +1,5 @@
 from connection import connection
 from decimal import Decimal
-#обернуть в класс
 
 
 def check_if_employee_exists(id: int) -> bool:
@@ -20,81 +19,86 @@ def check_if_employee_exists(id: int) -> bool:
             FROM employees
             WHERE id = %s);
             """, (id,)
-        )
+                       )
         return cursor.fetchone()[0]
 
-def create_employee(id: int, first_name: str, last_name: str, age: int, department: str, salary: Decimal):
-    """
-    Добавление нового сотрудника в БД
 
-    Args:
-        id(int) : уникальный id сотрудника
-        first_name(str): имя сотрудника
-        last_name(str): фамилия сотрудника
-        age(int): возраст сотрудника
-        department(str): подразделение, в котором работает сотрудник
-        salary(Decimal): зарплата сотрудника
+class Service_functions:
     """
-    with connection.cursor() as cursor:
-        if not(check_if_employee_exists(id)):
-            query = "INSERT INTO employees(id, first_name, last_name, age, department, salary) VALUES (%s, %s, %s, %s, %s, %s)"
-            cursor.execute(query, (id, first_name, last_name, age, department, salary))
-            connection.commit()
-        #else:
-            #print('Сотрудник с таким id уже существует')
+    Класс с методами для сервисной работы с БД
+    """
+    def create_employee(id: int, first_name: str, last_name: str, age: int, department: str, salary: Decimal):
+        """
+        Добавление нового сотрудника в БД
 
-def get_full_list_employees(per_page: int, offset: int) -> [tuple]:
-    """
-    Печатает список всех сотрудников
+        Args:
+            id(int) : уникальный id сотрудника
+            first_name(str): имя сотрудника
+            last_name(str): фамилия сотрудника
+            age(int): возраст сотрудника
+            department(str): подразделение, в котором работает сотрудник
+            salary(Decimal): зарплата сотрудника
+        """
+        with connection.cursor() as cursor:
+            if not(check_if_employee_exists(id)):
+                query = "INSERT INTO employees(id, first_name, last_name, age, department, salary) VALUES (%s, %s, %s, %s, %s, %s)"
+                cursor.execute(query, (id, first_name, last_name, age, department, salary))
+                connection.commit()
+            #else:
+                #print('Сотрудник с таким id уже существует')
 
-    Args:
-        per_page(int): сколько строк вывести
-        offset(int): сколько строк пропустить от начала списка и затем начать выводить строки
-    Return:
-        list of tuples - список кортежей с данными о сотрудниках
-    """
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """ 
-            SELECT * 
-            FROM employees
-            ORDER BY id
-            LIMIT %s OFFSET %s;
-            """, (per_page, offset,))
-        return cursor.fetchall()
+    def get_full_list_employees(per_page: int, offset: int) -> [tuple]:
+        """
+        Печатает список всех сотрудников
 
-def update_data_employee(id: int, dates: [dict]):
-    """
-    Обновляет данные сотрудника, принимая на вход его id и список из словарей {'column': 'value'}
+        Args:
+            per_page(int): сколько строк вывести
+            offset(int): сколько строк пропустить от начала списка и затем начать выводить строки
+        Return:
+            list of tuples - список кортежей с данными о сотрудниках
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """ 
+                SELECT * 
+                FROM employees
+                ORDER BY id
+                LIMIT %s OFFSET %s;
+                """, (per_page, offset,))
+            return cursor.fetchall()
 
-    Args:
-        id(int): уникальный id сотрудника
-        dates([dict]): список словарей {'column': 'value'}
-    """
-    with connection.cursor() as cursor:
-        if check_if_employee_exists(id):
-            for data in dates:
-                for key, value in data.items():
-                    # Обновление данных
-                    update = f"UPDATE employees SET {key} = %s WHERE id = %s"
-                    cursor.execute(update, (value, id,))
-                    connection.commit()
-        else:
-            print('Сотрудника с таким id не существует')
+    def update_data_employee(id: int, dates: [dict]):
+        """
+        Обновляет данные сотрудника, принимая на вход его id и список из словарей {'column': 'value'}
 
-def delete_employee(id: int):
-    """
-    Удаляет сотрудника из БД по его уникальному id
+        Args:
+            id(int): уникальный id сотрудника
+            dates([dict]): список словарей {'column': 'value'}
+        """
+        with connection.cursor() as cursor:
+            if check_if_employee_exists(id):
+                for data in dates:
+                    for key, value in data.items():
+                        # Обновление данных
+                        update = f"UPDATE employees SET {key} = %s WHERE id = %s"
+                        cursor.execute(update, (value, id,))
+                        connection.commit()
+            else:
+                print('Сотрудника с таким id не существует')
 
-    Args:
-        id(int): уникальный id сотрудника
-    """
-    with connection.cursor() as cursor:
-        if check_if_employee_exists(id):
-            delete = "DELETE FROM employees WHERE id = %s;"
-            cursor.execute(delete, (id,))
-            connection.commit()
-        else:
-            print('Сотрудника с таким id не существует')
+    def delete_employee(id: int):
+        """
+        Удаляет сотрудника из БД по его уникальному id
+
+        Args:
+            id(int): уникальный id сотрудника
+        """
+        with connection.cursor() as cursor:
+            if check_if_employee_exists(id):
+                delete = "DELETE FROM employees WHERE id = %s;"
+                cursor.execute(delete, (id,))
+                connection.commit()
+            else:
+                print('Сотрудника с таким id не существует')
 
 
